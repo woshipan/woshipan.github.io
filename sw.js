9f63f49ee6c547cfe6884c6131bcfddb1d9b1662
woshipan <2420940480@qq.com>
2024-12-06 13:04:28 +0800

    Site updated: 2024-12-06 13:04:28

--- sw.js
+++ sw.js
@@ -5,7 +5,7 @@ const preCache = [
   "/css/style.css",
   "/js/script.js",
 ];
-const VERSION = "1733458719444";
+const VERSION = "1733461460974";
 const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
